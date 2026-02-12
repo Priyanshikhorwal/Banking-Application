c3c8997ee4d26237c91374ed791ffd8476e29d88
Priyanshikhorwal <priyanshikhorwal0905@gmail.com>
2026-02-12 23:10:56 +0530

    first commit

---
+++ bankingApplication/src/main/java/com/pri/bankingwebapp/bankingApplication/mapper/AccountMapper.java
@@ -0,0 +1,27 @@
+package com.pri.bankingwebapp.bankingApplication.mapper;
+
+import com.pri.bankingwebapp.bankingApplication.dto.AccountDto;
+import com.pri.bankingwebapp.bankingApplication.entity.Account;
+import com.pri.bankingwebapp.bankingApplication.repository.AccountRepository;
+import org.apache.catalina.mapper.Mapper;
+
+public class AccountMapper{
+    public static Account mapToAccount(AccountDto accountdto) {
+        Account account = new Account(
+                accountdto.getId(),
+                accountdto.getAccountHolderName(),
+                accountdto.getBalance()
+        );
+        return account;
+
+    }
+    public static AccountDto mmapToAccountDto(Account account) {
+        AccountDto accountDto = new AccountDto(
+                account.getId(),
+                account.getAccountHolderName(),
+                account.getBalaance()
+        );
+        return accountDto;
+    }
+
+}
