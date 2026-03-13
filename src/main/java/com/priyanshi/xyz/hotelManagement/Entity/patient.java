134e0d0db8ada467a94d7cb4802cc6827ab4cc11
PriyanshiKhorwal <priyanshikhorwal0905@gmail.com>
2026-03-13 18:43:50 +0530

    adding exception in patient entity

--- src/main/java/com/priyanshi/xyz/hotelManagement/Entity/patient.java
+++ src/main/java/com/priyanshi/xyz/hotelManagement/Entity/patient.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -19,12 +20,19 @@ public class patient {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
-
+    @NotBlank
     private String name;
+    @Past
     private LocalDate dob;
+    @NotNull
+    @Max(100)
     private String address;
+    @Email
+    @NotBlank
     private String email;
+    @Size(min=10, max=10)
     private long phoneNumber;
+    @NotNull
     private String gender;
 
 
