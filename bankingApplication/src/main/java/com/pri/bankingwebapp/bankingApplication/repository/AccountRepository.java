4dab4af729f81c4433be210fd52ade54a901f604
PriyanshiKhorwal <priyanshikhorwal0905@gmail.com>
2026-04-05 17:18:27 +0530

    structure chnages

--- bankingApplication/src/main/java/com/pri/bankingwebapp/bankingApplication/repository/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package com.pri.bankingwebapp.bankingApplication.repository;
-
-import com.pri.bankingwebapp.bankingApplication.entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account,Long> {
-
-}
